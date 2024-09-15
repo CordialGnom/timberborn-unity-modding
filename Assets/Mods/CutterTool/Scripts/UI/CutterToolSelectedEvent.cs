4edffd40f728ff2dc26b8e47196e1fa547926fa4
CordialGnom <cordialgnom@gmail.com>
2024-09-15 22:24:18 +0200

    added new events for cutter tool

---
+++ Assets/Mods/CutterTool/Scripts/UI/CutterToolSelectedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Cordial.Mods.CutterTool.Scripts;
+
+namespace Cordial.Mods.CutterTool.Scripts.UI
+{
+    public class CutterToolSelectedEvent
+    {
+        public CutterToolService CutterToolService { get; }
+
+        public CutterToolSelectedEvent(CutterToolService cutterToolService)
+        {
+            this.CutterToolService = cutterToolService;
+        }
+
+    }
+}
---
+++ Assets/Mods/CutterTool/Scripts/UI/CutterToolUnselectedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Cordial.Mods.CutterTool.Scripts;
+
+namespace Cordial.Mods.CutterTool.Scripts.UI
+{
+    public class CutterToolUnselectedEvent
+    {
+        public CutterToolService CutterToolService { get; }
+
+        public CutterToolUnselectedEvent(CutterToolService cutterToolService)
+        {
+            this.CutterToolService = cutterToolService;
+        }
+
+    }
+}
