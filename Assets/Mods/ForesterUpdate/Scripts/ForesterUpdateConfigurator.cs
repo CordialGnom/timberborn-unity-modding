69c99d72e1f62ac08a4e046c4a021c45862014b1
CordialGnom <cordialgnom@gmail.com>
2024-09-24 15:21:53 +0200

    added forester project setup

---
+++ Assets/Mods/ForesterUpdate/Scripts/ForesterUpdateConfigurator.cs
@@ -0,0 +1,15 @@
+using Bindito.Core;
+
+namespace Cordial.Mods.ForesterUpdate.Scripts
+{
+    [Context("Game")]
+    public class CutterToolConfigurator : IConfigurator {
+
+        public void Configure(IContainerDefinition containerDefinition)
+        {
+
+        }
+
+
+    }
+}
---
+++ Assets/Mods/ForesterUpdate/Scripts/ForesterUpdateLogger.cs
@@ -0,0 +1,14 @@
+﻿using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace Cordial.Mods.ForesterUpdate.Scripts 
+{
+    internal class ForesterUpdateLogger : IModStarter 
+    {
+
+        public void StartMod()
+        {
+            // required to start up mod
+        }
+    }
+}
