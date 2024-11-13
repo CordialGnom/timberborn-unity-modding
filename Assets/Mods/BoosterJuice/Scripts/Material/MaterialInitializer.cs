f8d9acb78c2919f27020a2fcaff1285ad0973944
CordialGnom <cordialgnom@gmail.com>
2024-11-13 22:46:28 +0100

    updated material visualization and released mod

--- Assets/Mods/BoosterJuice/Scripts/Material/MaterialInitializer.cs
+++ Assets/Mods/BoosterJuice/Scripts/Material/MaterialInitializer.cs
@@ -8,8 +8,9 @@ namespace Cordial.Mods.BoosterJuice.Scripts.Material
     public class MaterialInitializer : ILoadableSingleton
     {
         IMaterialRepository _materialRepository;
-        Color fertilizerColor = new Color(0.58f, 0f, 0.42f, 1.0f);
-        Color fertilizerEmissionColor = new Color(0.58f, 0f, 0.42f, 0.01f);
+        Color fertilizerColor = new Color(0.6f, 0.1f, 0.42f, 1.0f);
+        Color fertilizerEmissionColor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        Color fertilizerEmissionMapColor = new Color(0.04f, 0f, 0.4f, 1.0f);
 
         public MaterialInitializer(IMaterialRepository materialRepository)
         {
@@ -26,15 +27,17 @@ namespace Cordial.Mods.BoosterJuice.Scripts.Material
                 && (fertilizer != null))
             {
                 // todo Cord: further tests of changing color properties required. 
-                fertilizer.CopyPropertiesFromMaterial(extract);
                 fertilizer.shader = extract.shader;
                 // disable foam, suggestion based on Tobbert/Emberpelt code
                 //fertilizer.SetVector("_FoamColor", fertilizerEmissionColor);
                 //fertilizer.SetFloat("_FoamIntensity", 0.0f);
 
+                fertilizer.CopyPropertiesFromMaterial(extract);
+
                 fertilizer.SetColor("_Color", fertilizerColor);
-                //fertilizer.SetColor("_EmissionColor", fertilizerEmissionColor);
-                fertilizer.SetVector("_FoamColor", fertilizerEmissionColor);
+                fertilizer.SetColor("_EmissionColor", fertilizerEmissionColor);
+                fertilizer.SetColor("_EmissionMapColor", fertilizerEmissionMapColor);
+                fertilizer.SetVector("_FoamColor", fertilizerColor);
                 fertilizer.SetFloat("_FoamIntensity", 0.005f);
             }
             else
